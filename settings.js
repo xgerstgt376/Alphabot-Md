11286fc6dacbc152d9b9ffe0bf46fb0c8b9d7c68
xgerstgt376 <127000841+xgerstgt376@users.noreply.github.com>
2023-03-21 00:13:18 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -35,30 +35,30 @@ global.groupOnly = false //bot hanya bisa di gunakan dalam grup
 //language
 //Available in indonesia , english & spanyol 
 //You can request others language 
-global.language = indonesia //change indonesia to english if you don't understand the language used by the bot
+global.language = english //change indonesia to english if you don't understand the language used by the bot
 //global.language = english // use this for English language
 //global.language = spanyol // use this for Spanyol language
 
 // Other
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
+global.botname = "✯᚛ᷝ ͣ ͫ𝓀𝒾𝒷𝑜͢͢͢𝓉 ͭ ͪ᚜ͤ✯" //namabot kalian
+global.ownername = "✯᭄⋆ͥ𝕻𝖊ͣ𝖓ͫ𝖓𝖞ͣ𝖜ͨ𝖎ᷝ𝖘ⷪ𝖊ᷱ⋆ᷠ࿐" //nama kalian
 global.myweb = "https://s.id/zeeoneofc" //bebas asal jan hapus
 global.youtube = "https://youtube.com/c/@ZeeoneOfc" //bebas asal jan hapus
 global.myweb2 = "https://s.id/zeeoneofc" //bebas
 global.email = "zeeoneofc@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['62887435047326'] //premium user
-global.owner = ["62887435047326", "62821931157232"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+"+ownernomer //nmr wa kalian
+global.premium = ['4917621050498'] //premium user
+global.owner = ["4917673231977", "4917621050498"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "4917673231977" // nomor wa kalian
+global.ownernomerr = "+"+4917621050498 //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome = "https://telegra.ph/file/edca4b513ddf29fd52c0e.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.thumbGif = 'https://telegra.ph/file/e90bef3779d3c5812a5d1.mp4'
-global.packname = '© ' + botname //sticker wm ubah
-global.author = 'Created By ' + ownername //sticker wm ganti nama kalian
+global.packname = '© ' + ✯᚛ᷝ ͣ ͫ𝓀𝒾𝒷𝑜͢͢͢𝓉 ͭ ͪ᚜ͤ✯ //sticker wm ubah
+global.author = 'Created By ' + ✯᭄⋆ͥ𝕻𝖊ͣ𝖓ͫ𝖓𝖞ͣ𝖜ͨ𝖎ᷝ𝖘ⷪ𝖊ᷱ⋆ᷠ࿐ //sticker wm ganti nama kalian
 global.sessionName = 'session'
 //type menu for menu
 global.typemenu = 'templateImage' //
